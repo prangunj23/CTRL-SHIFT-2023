92219ab76b2b6a0a042ae7d51077093e33be4452
unknown <prangunj23@ncs.charter.k12.de.us>
2023-03-30 21:22:15 -0400

    Able to get vectors from OpenAi, not able to compare them with pinecone

--- server/index.js
+++ server/index.js
@@ -1,28 +1,52 @@
 import { PineconeClient  } from "@pinecone-database/pinecone";
-import express from "express";
+import express, { query } from "express";
+import axios from "axios";
+const app = express();
 
-const app = express()
-
+const url = 'https://api.openai.com/v1/embeddings';
+const apiKey = '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'
+const model = 'text-embedding-ada-002';
 const pinecone = new PineconeClient();
-    await pinecone.init({ 
-        environment: "us-east-1-aws",
-        apiKey: "83767c83-b7d4-4329-ad26-19496aac7ec7",
+await pinecone.init({ 
+    environment: "us-east-1-aws",
+    apiKey: "83767c83-b7d4-4329-ad26-19496aac7ec7",
 }); 
-const index = pinecone.Index("example-index");
-const queryRequest = await index.fetch({
-    vector: [0],
-    topK: 5,
-    includeValues: true,
-    includeMetadata: true,
+ 
+
+
+const darequest = () => {
+    const body = {
+        "input": "cross-product",
+        "model": model,
+    };
+    axios.post(url, body, {
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${apiKey}`
+        }
+    }).then((res) => {
+        const embeddings = res.data['data'][0]['embedding'];
+        async () => {
+            const index = pinecone.Index("mathsearchengine");
+            const queryResponse = await index.query({
+                query: {
+                    vector: embeddings,
+                    topK: 5,
+                    includeValues: true,
+                },
+                namespace: "",
+            });
+            console.log(queryResponse);
+        }
+        
+    });
+};
+app.get('/', (req, res) => {
+    
+    res.send(darequest()); 
 });
-const queryRespone = await index.query({ queryRequest });
-const Description = await pinecone.describeIndex({
-    indexName: "mathsearchengine"
-}); 
-
-
 app.listen(3001, () => {
-    console.log(queryRespone);
+    console.log(darequest());
 });
 
 
